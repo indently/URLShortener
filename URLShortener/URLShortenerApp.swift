d8bbd5d9563374a11ab07249426db56493dd79fa
Code Palace <codepalace@fastmail.se>
2022-02-11 09:06:34 +0100

    Initial Commit

---
+++ URLShortener/URLShortenerApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLShortenerApp.swift
+//  URLShortener
+//
+//  Created by Federico on 11/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct URLShortenerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
